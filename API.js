746e72c44fbc4cacb1dc9400408bfdf33a4b1ffd
Tommy Waalkes <Tommy@grandcircus.co>
2018-05-15 09:51:19 -0400

    Updated API.js

--- API.js
+++ API.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    $.get("https://www.reddit.com/r/aww/.json").then(function (json) {
+    let subreddit = "aww";//= prompt("please enter a subreddit name");
+    $.get(`https://www.reddit.com/r/${subreddit}/.json`).then(function (json) {
     let text = json.data.children;    
     
     //$("#output").text(text.toString());
     let target = $("#output");
         //list.html(data);
         console.log(text[0].data.author);
-        for (i=0; i<text.length; i++) {
+        for (i=0; i<10; i++) {
             console.log(text[i].data.author);
             target.append(
-            `<a href=${text[i]}>
+            `<a href=${text[i].data.url}>
             <h2>${text[i].data.title}</h2>
             <p> Author: ${text[i].data.author}</p>
             <img src="${text[i].data.thumbnail}"/>
